e21e4c8eff3b6ca9beb1dc672d2753c67c66ccfb
medad <padmedad@gmail.com>
2012-11-16 03:31:18 -0500

    added a Users controller and layout links tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
 #about , contact, help home
   root :to => 'pages#home'
 
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
 
   #get "pages/home" covered
   # get "pages/contact" --covered
