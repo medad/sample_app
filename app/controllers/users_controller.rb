d6da7180657f80c1a12985604b2a90ef09c53738
medad <padmedad@gmail.com>
2012-12-01 15:15:59 -0500

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,19 @@ class UsersController < ApplicationController
   	@title = "Sign up"
   end
 
+  def create
+    # raise params[:user].inspect
+    @user = User.new(params[:user])
+    if @user.save      
+      #handle the successful save
+      #flash[:success] = "Welcome to the Sample App!" //gai note, can combime with redirect_to
+      #redirect_to  user_path(@user) //gai notes; can do also @user without user_path
+      redirect_to @user, :flash => {:success => "Welcome to the Sample App!"}
+    else 
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
 
 end
